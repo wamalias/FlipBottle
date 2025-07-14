09c9348d59ff5eb0436694e7249389082eb643b1
wamalias <wardamalias@gmail.com>
2025-07-14 10:54:13 +0700

    Initial commit

---
+++ FlipBottleTests/FlipBottle2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FlipBottle2Tests.swift
+//  FlipBottle2Tests
+//
+//  Created by Wardatul Amalia Safitri on 14/07/25.
+//
+
+import Testing
+@testable import FlipBottle2
+
+struct FlipBottle2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
